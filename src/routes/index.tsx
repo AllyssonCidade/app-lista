e9ab0077c74a8f19945ce109708106904d4bcbc7
AllyssonCidade <allyssoncidade@gmail.com>
2024-09-24 17:41:41 -0300

    style: Removido a barra de status superior

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ import { SQLiteProvider } from 'expo-sqlite';
 import { initializeDatabase } from '../database/initializeDatabase';
 import { SplashScreen } from '../screens/splashScreen';
 import { Login } from '../screens/login';
+import { StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator<PropsStackRoutes>();
 
@@ -15,6 +16,7 @@ const AppNavigation = () => {
   return (
     <SQLiteProvider databaseName="myDatabase.db" onInit={initializeDatabase}>
       <AuthProvider>
+      <StatusBar hidden barStyle={'dark-content'} backgroundColor="#8fe1d745" />
         <Stack.Navigator >
           <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false }} />
           <Stack.Screen name="Login" component={Login} options={{headerShown: false }} />
